bfafff6a33c5b414c8550e42ad23cfb61ccee27f
JeevanRaj <kavijeevanraj@gmail.com>
2024-06-05 19:09:27 +0530

    implemented the structure 5-6-24

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,12 +7,12 @@ import { SiFuturelearn } from "react-icons/si";
 import { SiOpenaccess } from "react-icons/si";
 import { CgProfile } from "react-icons/cg";
 import { IoArrowForward } from "react-icons/io5";
-import Submenu from "./Submenu.js"
+import Submenu from "./Submenu.js";
 import Logo from "../assets/images/logo.svg";
 import HamburgerButton from "./HamburgerMenuButton/HamburgerButton";
 
 const Sidebar = (MenuObj) => {
-  const [openSubmenu,setSubMenu] = useState(-1);
+  const [openSubmenu, setSubMenu] = useState(-1);
   const [open, setOpen] = useState(true);
   const [mobileMenu, setMobileMenu] = useState(false);
   const location = useLocation();
@@ -24,23 +24,22 @@ const Sidebar = (MenuObj) => {
     { title: "Signin", path: "/login", src: <SiOpenaccess />, gap: "true" },
   ];
 
-const getLabelForCurrentPath = (menu)=>{
-  for (const smenu of menu.children) {
-    if (smenu.children) {
-      for (const i of smenu.children) {
-        if (i.path === location.pathname) {
-          return i.label;
+  const getLabelForCurrentPath = (menu) => {
+    for (const smenu of menu.children) {
+      if (smenu.children) {
+        for (const i of smenu.children) {
+          if (i.path === location.pathname) {
+            return i.label;
+          }
+        }
+      } else {
+        if (smenu.path === location.pathname) {
+          return smenu.label;
         }
       }
-    } else {
-      if (smenu.path === location.pathname) {
-        return smenu.label;
-      }
     }
-  }
-  return null;
-
-}
+    return null;
+  };
   return (
     <>
       <div
@@ -51,7 +50,7 @@ const getLabelForCurrentPath = (menu)=>{
         <BsArrowLeftCircle
           className={`${
             !open && "rotate-180"
-          } absolute text-3xl bg-white fill-slate-800  rounded-full cursor-pointer top-9 -right-4 dark:fill-gray-400 dark:bg-gray-800`}
+          } absolute text-3xl bg-white fill-slate-800  rounded-full cursor-pointer top-2 -right-4 dark:fill-gray-400 dark:bg-gray-800`}
           onClick={() => setOpen(!open)}
         />
         <Link to="/">
@@ -89,42 +88,50 @@ const getLabelForCurrentPath = (menu)=>{
             ) : (
               <>
                 <li
-                  key = {index}
-                  className={`relative   p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
+                  key={index}
+                  className={`${
+                    menu.children.length < 2 ? "relative" : ""
+                  } p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
                     ${menu.gap ? "mt-9" : "mt-2"} ${
                     location.pathname === menu.path &&
                     "bg-gray-200 dark:bg-gray-700"
                   }`}
-                  onMouseEnter={()=>{setSubMenu(index)}}
-                  onMouseLeave={()=>{setSubMenu(-1)}}
+                  onMouseEnter={() => {
+                    setSubMenu(index);
+                  }}
+                  onMouseLeave={() => {
+                    setSubMenu(-1);
+                  }}
                 >
                   <div className="flex items-center gap-x-6">
-                  <span>{menu.icon}</span>
-                  <span
-                    className={`${
-                      !open && "hidden"
-                    }  origin-left duration-300 hover:block`}
-                  >
-                    {menu.label}
-                    
-                    <span >
-                      <Submenu  menu={menu.children} location={location} open = {open} openSubmenu = {openSubmenu} keyNum={index}/>
+                    <span>{menu.icon}</span>
+                    <span
+                      className={`${
+                        !open && "hidden"
+                      }  origin-left duration-300 hover:block`}
+                    >
+                      {menu.label}
+
+                      <span>
+                        <Submenu
+                          menu={menu.children}
+                          location={location}
+                          open={open}
+                          openSubmenu={openSubmenu}
+                          keyNum={index}
+                        />
+                      </span>
                     </span>
-                    
-                    
-
-                  </span>
-                  <IoArrowForward className={`${
-                      !open && "hidden"
-                    } ml-auto`}/>
+                    <IoArrowForward
+                      className={`${!open && "hidden"} ml-auto`}
+                    />
                   </div>
-                  {getLabelForCurrentPath(menu)
-                  &&
-                  <span className="block mt-4 p-3 text-center rounded-lg bg-gray-200 dark:bg-gray-700">
-                    {getLabelForCurrentPath(menu)}
-                  </span> }
+                  {getLabelForCurrentPath(menu) && (
+                    <span className="block mt-4 p-3 text-center rounded-lg bg-gray-200 dark:bg-gray-700">
+                      {getLabelForCurrentPath(menu)}
+                    </span>
+                  )}
                 </li>
-
               </>
             )
           )}
@@ -137,7 +144,7 @@ const getLabelForCurrentPath = (menu)=>{
           mobileMenu={mobileMenu}
         />
       </div>
-      <div className="sm:hidden">
+      {/* <div className="sm:hidden">
         <div
           className={`${
             mobileMenu ? "flex" : "hidden"
@@ -159,6 +166,98 @@ const getLabelForCurrentPath = (menu)=>{
               </span>
             </Link>
           ))}
+        </div>
+      </div> */}
+      <div className="sm:hidden">
+        <div
+          className={`${
+            mobileMenu ? "flex" : "hidden"
+          } absolute z-50 flex-col items-center self-end py-8 mt-16 space-y-6 font-bold sm:w-auto left-6 right-6 dark:text-white  bg-gray-50 dark:bg-slate-800 drop-shadow md rounded-xl`}
+        >
+          <Link className="w-full" to="/" onClick={() => setMobileMenu(false)}>
+            <div className={`flex justify-center items-center relative`}>
+              <img src={Logo} alt="" className="pl-2 mr-1" />
+              {open && (
+                <span className="text-xl font-medium whitespace-nowrap dark:text-white">
+                  Side bar
+                </span>
+              )}
+            </div>
+          </Link>
+
+          <ul className="relative w-full px-1">
+            {MenuObj.Menu.map((menu, index) =>
+              !menu.children ? (
+                <Link to={menu.path} key={index} onClick={() => setMobileMenu(false)}>
+                  <li
+                    className={`flex items-center gap-x-6 p-2 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
+                        ${menu.gap ? "mt-9" : "mt-2"} ${
+                      location.pathname === menu.path &&
+                      "bg-gray-200 dark:bg-gray-700"
+                    }`}
+                  >
+                    <span>{menu.icon}</span>
+                    <span
+                      className={`${
+                        !open && "hidden"
+                      } origin-left duration-300 hover:block`}
+                    >
+                      {menu.label}
+                    </span>
+                  </li>
+                </Link>
+              ) : (
+                <>
+                  <li
+                    key={index}
+                    className={`${
+                      menu.children.length < 2 ? "relative" : ""
+                    } p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
+                    ${menu.gap ? "mt-9" : "mt-2"} ${
+                      location.pathname === menu.path &&
+                      "bg-gray-200 dark:bg-gray-700"
+                    }`}
+                    onClick={() => {
+                      (openSubmenu===index)?setSubMenu(-1):setSubMenu(index);
+                    }}
+                   
+                  >
+                    <div className="flex items-center gap-x-6">
+                      <span>{menu.icon}</span>
+                      <span
+                        className={`${
+                          !open && "hidden"
+                        }  origin-left duration-300 hover:block`}
+                      >
+                        {menu.label}
+
+                        
+                      </span>
+                      <IoArrowForward
+                        className={`ml-auto ${openSubmenu===index?"rotate-90":""}`}
+                      />
+                    </div>
+                    <span>
+                          <Submenu
+                            menu={menu.children}
+                            location={location}
+                            open={open}
+                            openSubmenu={openSubmenu}
+                            keyNum={index}
+                            mobileMenu = {mobileMenu}
+                            setMobileMenu={setMobileMenu}
+                          />
+                        </span>
+                    {getLabelForCurrentPath(menu) && (
+                      <span className="block mt-4 p-3 text-center rounded-lg bg-gray-200 dark:bg-gray-700">
+                        {getLabelForCurrentPath(menu)}
+                      </span>
+                    )}
+                  </li>
+                </>
+              )
+            )}
+          </ul>
         </div>
       </div>
     </>
--- src/components/Submenu.js
+++ src/components/Submenu.js
@@ -3,78 +3,162 @@ import { Link } from "react-router-dom";
 
 function Submenu(props) {
   return (
-    <div
-      className={`${
-        props.openSubmenu === props.keyNum
-          ? "w-60 pt-6 absolute -top-4 -right-60 sm:block duration-300 bg-gray-100 border-r border-gray-200 dark:border-gray-600 p-5 dark:bg-slate-800"
-          : "hidden"
-      } `}
-    >
-      <ul>
-        {props.menu.map((menu, index) =>
-          !menu.children ? (
-            <Link to={menu.path} key={index}>
-              <li
-                className={`flex items-center gap-x-6 p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
+    <>
+      <div
+        className={`${
+          props.openSubmenu === props.keyNum
+            ? "w-full sm:w-60 pt-3  sm:absolute sm:-top-2 sm:-right-60  sm:block duration-300  sm:bg-gray-100 sm:border-r sm:border-gray-200 dark:border-gray-600 p-5 dark:bg-slate-800"
+            : "hidden"
+        } `}
+      >
+        <ul>
+          {props.menu.map((menu, index) =>
+            !menu.children ? (
+              <Link to={menu.path} key={index} onClick={() => props.setMobileMenu(false)}>
+                <li
+                  className={`flex items-center gap-x-6 p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
                         ${menu.gap ? "mt-9" : "mt-2"} ${
-                  props.location.pathname === menu.path &&
-                  "bg-gray-200 dark:bg-gray-700"
-                }`}
-              >
-                <span>{menu.icon}</span>
-                <span
-                  className={`${
-                    !props.open && "hidden"
-                  } origin-left duration-300 hover:block`}
+                    props.location.pathname === menu.path &&
+                    "bg-gray-200 dark:bg-gray-700"
+                  }`}
                 >
-                  {menu.label}
-                </span>
-              </li>
-            </Link>
-          ) : (
-            <>
-              <li className={"items-center text-black font-normal rounded-lg cursor-default mt-2 "}>
-                <div className="border-b-2 border-solid border-black">
-                <span>{menu.icon}</span>
-                <span
-                  className={`${
-                    !props.open && "hidden"
-                  } ml-2 origin-left duration-300 `}
+                  <span>{menu.icon}</span>
+                  <span
+                    className={`${
+                      !props.open && "hidden"
+                    } origin-left duration-300 hover:block`}
+                  >
+                    {menu.label}
+                  </span>
+                </li>
+              </Link>
+            ) : (
+              <>
+                <li
+                  className={
+                    "items-center text-black font-normal rounded-lg cursor-default mt-2 "
+                  }
                 >
-                  {menu.label}
-                </span>
-                </div>
-                <span>
-                  <ul>
-                    
-                    {menu.children.map((menu, index) => (
-                      <Link to={menu.path} key={index}>
-                        <li
-                          className={`flex items-center gap-x-6 p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
+                  <div className="border-b-2 border-solid border-black">
+                    <span>{menu.icon}</span>
+                    <span
+                      className={`${
+                        !props.open && "hidden"
+                      } ml-2 origin-left duration-300 `}
+                    >
+                      {menu.label}
+                    </span>
+                  </div>
+                  <span>
+                    <ul>
+                      {menu.children.map((menu, index) => (
+                        <Link to={menu.path} key={index} onClick={() => props.setMobileMenu(false)}>
+                          <li
+                            className={`flex items-center gap-x-6 p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
                         ${menu.gap ? "mt-9" : "mt-2"} ${
-                            props.location.pathname === menu.path &&
-                            "bg-gray-200 dark:bg-gray-700"
-                          }`}
-                        >
-                          <span >{menu.icon}</span>
-                          <span
-                            className={`${
-                              !props.open && "hidden"
-                            } origin-left duration-300 hover:block`}
+                              props.location.pathname === menu.path &&
+                              "bg-gray-200 dark:bg-gray-700"
+                            }`}
                           >
-                            {menu.label}
-                          </span>
-                        </li>
-                      </Link>
-                    ))}
-                  </ul>
-                </span>
-              </li>
-            </>
-          )
-        )}
-      </ul>
-    </div>
+                            <span>{menu.icon}</span>
+                            <span
+                              className={`${
+                                !props.open && "hidden"
+                              } origin-left duration-300 hover:block`}
+                            >
+                              {menu.label}
+                            </span>
+                          </li>
+                        </Link>
+                      ))}
+                    </ul>
+                  </span>
+                </li>
+              </>
+            )
+          )}
+        </ul>
+      </div>
+      {/* Mobile menu */}
+      {/* <div className="">
+        <div
+          className={`${
+            props.openSubmenu === props.keyNum
+              ? "w-60 pt-6 absolute -top-2 -right-60 hidden sm:block duration-300 bg-gray-100 border-r border-gray-200 dark:border-gray-600 p-5 dark:bg-slate-800"
+              : "hidden"
+          } `}
+        >
+          <>hii</>
+          <ul>
+            {props.menu.map((menu, index) =>
+              !menu.children ? (
+                <Link to={menu.path} key={index}>
+                  <li
+                    className={`flex items-center gap-x-6 p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
+                        ${menu.gap ? "mt-9" : "mt-2"} ${
+                      props.location.pathname === menu.path &&
+                      "bg-gray-200 dark:bg-gray-700"
+                    }`}
+                  >
+                    <span>{menu.icon}</span>
+                    <span
+                      className={`${
+                        !props.open && "hidden"
+                      } origin-left duration-300 hover:block`}
+                    >
+                      {menu.label}
+                    </span>
+                  </li>
+                </Link>
+              ) : (
+                <>
+                  <li
+                    className={
+                      "items-center text-black font-normal rounded-lg cursor-default mt-2 "
+                    }
+                  >
+                    <div className="border-b-2 border-solid border-black">
+                      <span>{menu.icon}</span>
+                      <span
+                        className={`${
+                          !props.open && "hidden"
+                        } ml-2 origin-left duration-300 `}
+                      >
+                        {menu.label}
+                      </span>
+                    </div>
+                    <span>
+                      <ul>
+                        {menu.children.map((menu, index) => (
+                          <Link to={menu.path} key={index}>
+                            <li
+                              className={`flex items-center gap-x-6 p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
+                        ${menu.gap ? "mt-9" : "mt-2"} ${
+                                props.location.pathname === menu.path &&
+                                "bg-gray-200 dark:bg-gray-700"
+                              }`}
+                            >
+                              <span>{menu.icon}</span>
+                              <span
+                                className={`${
+                                  !props.open && "hidden"
+                                } origin-left duration-300 hover:block`}
+                              >
+                                {menu.label}
+                              </span>
+                            </li>
+                          </Link>
+                        ))}
+                      </ul>
+                    </span>
+                  </li>
+                </>
+              )
+            )}
+          </ul>
+        </div>
+      </div> */}
+    </>
   );
 }
 
